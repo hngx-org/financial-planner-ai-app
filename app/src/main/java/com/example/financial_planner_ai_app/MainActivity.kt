9468c078f78cb891f8c92ac665f043ba99685bfc
Eliud Githuku <138256399+githukueliud@users.noreply.github.com>
2023-09-30 00:45:17 +0300

    Sign up screen done (#3)
    * App theme set
    
    * Sign up screen

--- app/src/main/java/com/example/financial_planner_ai_app/MainActivity.kt
+++ app/src/main/java/com/example/financial_planner_ai_app/MainActivity.kt
@@ -22,25 +22,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //app entry comes here
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FinancialplanneraiappTheme {
-        Greeting("Android")
-    }
-}
