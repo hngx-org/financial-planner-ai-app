4667cdfd660af34b18416610195b6d6d78cbf00f
Idongesit Samuel <robsonidongesitsamuel@gmail.com>
2023-09-30 11:31:34 +0100

    SplashScreen and Login Screen (#9)

---
+++ app/src/main/java/com/example/financial_planner_ai_app/ui/login_screen/loginScreen.kt
@@ -0,0 +1,169 @@
+package com.example.financial_planner_ai_app.login_screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.financial_planner_ai_app.R
+import com.example.financial_planner_ai_app.ui.theme.md_theme_light_onPrimary
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Login(
+    uiState: LoginState
+){
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.atm))
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever)
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+        Box(
+            contentAlignment = Alignment.TopCenter,
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.background)
+        ){
+            LaunchedEffect(key1 = progress){}
+            Column {
+                LottieAnimation(
+                    modifier = Modifier.size(400.dp),
+                    composition = composition,
+                    progress = {progress})
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.Bottom,
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.welcome),
+                        textAlign = TextAlign.Center,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                }
+
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column(modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally){
+            OutlinedTextField(
+                value = uiState.emailAddress,
+                onValueChange = {},
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                placeholder = {
+                    Text(text = stringResource(id = R.string.hint_email))
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Next,
+                    keyboardType = KeyboardType.Email
+                )
+            )
+
+            OutlinedTextField(
+                value = uiState.password,
+                onValueChange = {},
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                placeholder = {
+                    Text(text = stringResource(id = R.string.hint_password))
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done,
+                    keyboardType = KeyboardType.Email
+                )
+            )
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.login),
+                    color = md_theme_light_onPrimary,
+                    modifier = Modifier.padding(16.dp)
+                )
+                
+            }
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(0.5.dp)
+            ){
+                Text(
+                    text = stringResource(id = R.string.dont_have)
+                )
+                TextButton(
+                    onClick = {},
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.click),
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewLoginScreen(){
+    Login(
+        LoginState(
+            emailAddress = "",
+            password = ""
+        )
+    )
+}
+
+
---
+++ app/src/main/java/com/example/financial_planner_ai_app/ui/login_screen/loginState.kt
@@ -0,0 +1,6 @@
+package com.example.financial_planner_ai_app.login_screen
+
+data class LoginState(
+    val emailAddress:String,
+    val password:String
+)
