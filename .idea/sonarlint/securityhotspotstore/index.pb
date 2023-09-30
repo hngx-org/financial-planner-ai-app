28bb63e1e34988adcdf5f34d51892718f818cd2c
mungai-codes <mokabea8@gmail.com>
2023-09-30 15:45:22 +0300

    Created the onboarding screen

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,17 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+‘
+aapp/src/main/java/com/example/financial_planner_ai_app/presentation/login/components/LottieAtm.kt,e/c/ec68acc16874a44175240a339f88d5e6f73a395c
+Š
+Zapp/src/main/java/com/example/financial_planner_ai_app/presentation/splash/SplashScreen.kt,6/9/69ab771170e541fb1f0235624d02a06fb27c6164
+ˆ
+Xapp/src/main/java/com/example/financial_planner_ai_app/presentation/login/loginScreen.kt,3/8/38d2d279e194f32f2ea55b634b3dccbbe08269f9
+‡
+Wapp/src/main/java/com/example/financial_planner_ai_app/presentation/login/loginState.kt,3/5/35e0fdcbdc6763f4955bdb4314a76cb59e88adda
+v
+Fapp/src/main/java/com/example/financial_planner_ai_app/MainActivity.kt,8/1/8190fd45cb678386badd56112a84fd9f928482f5
+’
+bapp/src/main/java/com/example/financial_planner_ai_app/presentation/onboarding/OnboardingScreen.kt,2/a/2a7e8977ee67236b9053740119823af7ac01ae29
+–
+fapp/src/main/java/com/example/financial_planner_ai_app/presentation/onboarding/components/PagerItem.kt,7/2/725ed1eab5186e0827a33f1a5ec0de370f2a01e8
---
+++ app/src/main/java/com/example/financial_planner_ai_app/presentation/onboarding/components/OnboardingButton.kt
@@ -0,0 +1,76 @@
+package com.example.financial_planner_ai_app.presentation.onboarding.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowRightAlt
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.financial_planner_ai_app.presentation.theme.FinancialplanneraiappTheme
+
+@Composable
+fun OnBoardingButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        modifier = modifier
+            .clickable { onClick() }
+            .fillMaxWidth(),
+        color = MaterialTheme.colorScheme.primaryContainer,
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Begin", fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(start = 16.dp),
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+            Box(
+                modifier = Modifier
+                    .padding(end = 32.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.primary)
+                    .size(54.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.ArrowRightAlt, contentDescription = null,
+                    modifier = Modifier.size(30.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun OnboardingButtonPreview() {
+    FinancialplanneraiappTheme {
+        OnBoardingButton(onClick = {})
+    }
+}
