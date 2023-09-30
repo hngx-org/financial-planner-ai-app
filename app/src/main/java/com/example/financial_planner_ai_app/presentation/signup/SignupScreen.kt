7a6dda921ea4a69ec57a0db630c24e886bdd8aae
Eliud Githuku <githukueliud@gmail.com>
2023-09-30 22:23:03 +0300

    Sign up page ui state, and also field validation done

--- app/src/main/java/com/example/financial_planner_ai_app/presentation/signup/SignupScreen.kt
+++ app/src/main/java/com/example/financial_planner_ai_app/presentation/signup/SignupScreen.kt
@@ -1,7 +1,7 @@
 package com.example.financial_planner_ai_app.presentation.signup
 
 
-import android.opengl.Visibility
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -18,8 +18,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
