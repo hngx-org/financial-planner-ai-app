c49736d67dc2f1fff6be6914e685023e97d3d329
HP <robsonidongesitsamuel@gmail.com>
2023-09-30 03:40:18 +0100

    SplashScreen and Login Screen

---
+++ app/src/main/java/com/example/financial_planner_ai_app/ui/login_screen/LottieAtm.kt
@@ -0,0 +1,22 @@
+package com.example.financial_planner_ai_app.login_screen
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.financial_planner_ai_app.R
+
+
+@Composable
+fun LottieATM() {
+
+
+
+}
