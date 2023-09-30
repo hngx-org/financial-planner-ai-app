e815b98f344840ba67e7d9061545c8f6bca48d0b
mungai-codes <mokabea8@gmail.com>
2023-09-30 13:09:30 +0300

    Setting up datastore preferences(tracking onboarding state).

---
+++ app/src/main/java/com/example/financial_planner_ai_app/data/repository/DataStoreRepository.kt
@@ -0,0 +1,34 @@
+package com.example.financial_planner_ai_app.data.repository
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import com.example.financial_planner_ai_app.data.local.ON_BOARDING_KEY
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+
+class DataStoreRepository(
+    private val userPreferenceStore: DataStore<Preferences>
+) {
+
+    suspend fun saveOnboardingState(onBoarded: Boolean) {
+        userPreferenceStore.edit { preferences ->
+            preferences[ON_BOARDING_KEY] = onBoarded
+        }
+    }
+
+    fun readOnboardingState(): Flow<Boolean> {
+        return userPreferenceStore.data
+            .catch { exception ->
+                if (exception is Exception) {
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }.map { preferences ->
+                preferences[ON_BOARDING_KEY] ?: false
+            }
+    }
+}
